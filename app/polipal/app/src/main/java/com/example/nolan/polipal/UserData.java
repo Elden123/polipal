3f64b13a91bde58a856767b570a040abddb6636f
BeijingStrongbow <ericdhiggins@gmail.com>
2017-11-04 09:27:10 -0500

    Working on Firebase

--- app/polipal/app/src/main/java/com/example/nolan/polipal/UserData.java
+++ app/polipal/app/src/main/java/com/example/nolan/polipal/UserData.java
@@ -18,17 +18,19 @@ public class UserData {
 
     private String[] policyInterests;
 
+    public UserData(){}
+
     public UserData(String name){
         this.name = name;
     }
+
+    public void setName(String name){ this.name = name; }
 
     public String getEmail(){
         return email;
     }
 
-    public void setEmail(String email){
-        this.email = email;
-    }
+    public void setEmail(String email){ this.email = email; }
 
     public String getPassword(){
         return email;
--- app/polipal/app/src/main/java/com/github/beijingstrongbow/Communication/FirebaseHandler.java
+++ app/polipal/app/src/main/java/com/github/beijingstrongbow/Communication/FirebaseHandler.java
@@ -1,8 +1,19 @@
 package com.github.beijingstrongbow.Communication;
 
 import com.example.nolan.polipal.UserData;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.StringBufferInputStream;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.SynchronousQueue;
 
 /**
  * Created by ericd on 11/4/2017.
@@ -26,6 +37,51 @@ public class FirebaseHandler {
         user.child("policyInterests").setValue(stringArrayToCSV(data.getPolicyInterests()));
     }
 
+    private final Semaphore lock = new Semaphore(0);
+
+    private final UserData data = new UserData();
+
+    public UserData retrieveUser(final String email, final String password){
+
+        usersRef.addListenerForSingleValueEvent(new ValueEventListener(){
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                System.out.println("here");
+                System.out.println("here");
+                System.out.println("here");
+                System.out.println("here");
+                System.out.println("here");
+                System.out.println("here");
+
+                lock.release();
+                /*for(DataSnapshot d : dataSnapshot.getChildren()){
+                    System.out.println(((String) d.child("email").getValue()) + " " + ((String) d.child("password").getValue()));
+                    if(((String) d.child("email").getValue()).equals(email) && ((String) d.child("password").getValue()).equals(password)){
+                        data.setName((String) d.child("name").getValue());
+                        data.setEmail(email);
+                        data.setPassword(password);
+                        data.setPolicyInterests(csvToStringArray((String) d.child("policyInterests").getValue()));
+                        data.setHobbies(csvToStringArray((String) d.child("hobbies").getValue()));
+                        data.setPoliticalParty((String) d.child("politicalParty").getValue());
+                    }
+                }*/
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                databaseError.toException().printStackTrace();
+            }
+        });
+        try{
+            lock.acquire();
+            System.out.println("in main thread");
+        }
+        catch(InterruptedException ex){}
+
+        return data;
+    }
+
     private String stringArrayToCSV(String[] array){
         StringBuilder output = new StringBuilder();
         for(int i = 0; i < array.length; i++){
@@ -35,5 +91,9 @@ public class FirebaseHandler {
         output.deleteCharAt(output.length() - 1);
 
         return output.toString();
+    }
+
+    private String[] csvToStringArray(String csv){
+        return csv.split(",");
     }
 }
