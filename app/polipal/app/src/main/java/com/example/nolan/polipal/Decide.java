bc7a3c0e217d81e17a5d10d8e44a309dd3ab6ebe
Unknown <nolanblankenau@gmail.com>
2017-11-03 22:05:39 -0500

    Skeleton of app

---
+++ app/polipal/app/src/main/java/com/example/nolan/polipal/Decide.java
@@ -0,0 +1,13 @@
+package com.example.nolan.polipal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Decide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_decide);
+    }
+}
---
+++ app/polipal/app/src/main/java/com/example/nolan/polipal/Home.java
@@ -0,0 +1,13 @@
+package com.example.nolan.polipal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
