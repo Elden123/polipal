b261fb0550ba60432e8c74a2ccab714d948e0ffb
Unknown <nolanblankenau@gmail.com>
2017-11-04 19:31:21 -0600

    Conversation.java pretty much works

--- app/polipal/app/src/main/java/com/example/nolan/polipal/Conversation.java
+++ app/polipal/app/src/main/java/com/example/nolan/polipal/Conversation.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -28,25 +30,80 @@ public class Conversation extends AppCompatActivity {
         String buffer = "";
 
         TextView matchedText = (TextView) findViewById(R.id.matchedText);
+        Button sendButton = (Button) findViewById(R.id.sendButton);
+        final EditText messageToSend = (EditText) findViewById(R.id.messageToSend);
+
 
         for(int i = 0; i < matchedOn.size(); i++) {
             buffer = matchedText.getText().toString();
             matchedText.setText(buffer + ", " + matchedOn.get(i));
         }
 
-        addMessage("asdf", true);
-        addMessage("asdf", false);
-        addMessage("asdf", false);
-        addMessage("asdf", false);
-        addMessage("asdf", true);
-        addMessage("asdf", false);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMyMessage(messageToSend.getText().toString());
+                messageToSend.setText("");
+            }
+        });
 
     }
 
+    public void showMyMessage(String message) {
+        addMessage(message, true);
+    }
+
+    public void showTheirMessage(String message) {
+        addMessage(message, false);
+    }
 
     public void addMessage(String message, boolean isUser) {
 
         LinearLayout LL = new LinearLayout(this);
+        TextView tv1 = new TextView(this);
+        LL.setOrientation(LinearLayout.HORIZONTAL);
+
+        LinearLayout.LayoutParams LLParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
+        LinearLayout.LayoutParams tv1Params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, Gravity.LEFT);
+
+        LLParams.weight = 1;
+        tv1Params.weight = 1;
+        tv1Params.setMargins(0,30,0,30);
+        tv1.setLayoutParams(tv1Params);
+
+        if(isUser) {
+            tv1.setTextColor(Color.parseColor("#48C5FC"));
+        } else {
+            tv1.setTextColor(Color.parseColor("#E8336F"));
+        }
+        tv1.setText(message);
+        tv1.setTextSize(18);
+        LL.setLayoutParams(LLParams);
+
+        LL.addView(tv1);
+
+        holderLayout = (LinearLayout) findViewById(R.id.lLayout);
+        holderLayout.addView(LL);
+
+
+
+
+    }
+
+    public ArrayList<String> getMatchedOn() {
+        ArrayList<String> matchedOn = new ArrayList<String>();
+        matchedOn.add("Gun Control");
+        matchedOn.add("Abortion");
+        matchedOn.add("Immigration");
+        matchedOn.add("Shopping");
+        matchedOn.add("Cooking");
+        matchedOn.add("Playing Sports");
+
+        return(matchedOn);
+    }
+
+    /*
+LinearLayout LL = new LinearLayout(this);
         TextView tv1 = new TextView(this);
         TextView tv2 = new TextView(this);
         LL.setOrientation(LinearLayout.HORIZONTAL);
@@ -61,6 +118,8 @@ public class Conversation extends AppCompatActivity {
         LLParams.weight = 1;
         tv1Params.weight = 1;
         tv2Params.weight = 1;
+        tv1Params.setMargins(0,30,0,30);
+        tv2Params.setMargins(0,30,0,30);
         tv1.setLayoutParams(tv1Params);
         tv2.setLayoutParams(tv2Params);
 
@@ -85,21 +144,5 @@ public class Conversation extends AppCompatActivity {
 
         holderLayout = (LinearLayout) findViewById(R.id.lLayout);
         holderLayout.addView(LL);
-
-
-
-
-    }
-
-    public ArrayList<String> getMatchedOn() {
-        ArrayList<String> matchedOn = new ArrayList<String>();
-        matchedOn.add("Gun Control");
-        matchedOn.add("Abortion");
-        matchedOn.add("Immigration");
-        matchedOn.add("Shopping");
-        matchedOn.add("Cooking");
-        matchedOn.add("Playing Sports");
-
-        return(matchedOn);
-    }
+     */
 }
